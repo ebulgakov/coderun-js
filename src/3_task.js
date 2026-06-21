fd60956da177635b1e30211b0693e7a70abe4a46
Eugene Bulgakov <zoom81@yandex.ru>
2026-06-21 20:56:00 +0400

    Поправлен фикс в 12 условии

--- src/3_task.js
+++ src/3_task.js
@@ -18,10 +18,41 @@ export function main(input) {
   const sumMatrix = Array.from({ length: rows }, () => new Array(cols).fill(0));
   const pathMatrix = Array.from({ length: rows }, () => new Array(cols).fill(""));
 
-  for (let rowIdx = 0; rowIdx < rows; rowIdx++) {
-    for (let colIdx = 0; colIdx < cols; colIdx++) {
-      const fromTop = sumMatrix[Math.max(0, rowIdx - 1)][colIdx];
-      const fromLeft = sumMatrix[rowIdx][Math.max(0, colIdx - 1)];
+  {
+    /*
+     // сделаем подготовку для вложенного for, чтобы не пришлось сравнивать < 0
+     x x x
+     x 0 0
+     x 0 0
+     */
+
+    // горизонтально считаем верхнюю строку
+    sumMatrix[0][0] = matrix[0][0];
+    for (let colIdx = 1; colIdx < cols; colIdx++) {
+      sumMatrix[0][colIdx] = sumMatrix[0][colIdx - 1] + matrix[0][colIdx];
+      pathMatrix[0][colIdx] = "R";
+    }
+
+    // вертикально считаем первый нижний столбец
+    for (let rowIdx = 1; rowIdx < rows; rowIdx++) {
+      sumMatrix[rowIdx][0] = sumMatrix[rowIdx - 1][0] + matrix[rowIdx][0];
+      pathMatrix[rowIdx][0] = "D";
+    }
+  }
+
+  /*
+   // обрабатываем построчно оставшиеся нули для x5 x6, x8 x9
+   x1 x2 x3
+   x4 x5 x6
+   x7 x8 x9
+   // если x2 > x4, то в x5 будет число x2 + x5
+   // если x3 < x5, то в x6 будет число x5 + x6
+   // в конечном счёте мы знаем что в x9 будет максимальная возможная сумма
+   */
+  for (let rowIdx = 1; rowIdx < rows; rowIdx++) {
+    for (let colIdx = 1; colIdx < cols; colIdx++) {
+      const fromTop = sumMatrix[rowIdx - 1][colIdx];
+      const fromLeft = sumMatrix[rowIdx][colIdx - 1];
 
       if (fromTop > fromLeft) {
         sumMatrix[rowIdx][colIdx] = fromTop + matrix[rowIdx][colIdx];
@@ -33,6 +64,10 @@ export function main(input) {
     }
   }
 
+  /*
+   благодаря тому, что на предыдущем шаге в pathMatrix записывалась история откуда вёлся подсчёт:
+   сверху или слева, то можно прокрутить историю в обратном направлении до 0, 0
+   */
   const foundPath = [];
   let rowsPath = rows - 1;
   let colsPath = cols - 1;
